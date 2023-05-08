fd0073bf7a7f067b90b7b8e130a6198ffa0e8c0e
BXb <1113855149@qq.com>
2023-05-08 16:48:50 +0800

    修改

--- PluginFrame/Plugins/message/public_message.py
+++ PluginFrame/Plugins/message/public_message.py
@@ -444,7 +444,7 @@ class ChoosePlugin(BaseComponentPlugin):
         elif mus_type == "kugou":
             res = requests.get(f"https://v.api.aa1.cn/api/kugou/?msg={music_name}&type={id_}")
             res_json = res.json()
-            if res_json.get("SongTitle"):
+            if res_json.get("PlayLink"):
                 # message = MessageSegment.music_custom(
                 #     url=res_json.get("PlayLink"),
                 #     audio_url=res_json.get("PlayLink"),
