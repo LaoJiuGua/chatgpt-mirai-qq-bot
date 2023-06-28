250df884394afbe856d6bdf2c819fd484603e8f0
BXb <1113855149@qq.com>
2023-06-28 18:17:26 +0800

    修改

--- PluginFrame/Plugins/video/video_piugin.py
+++ PluginFrame/Plugins/video/video_piugin.py
@@ -26,7 +26,8 @@ class DouYinBellePlugin(BaseComponentPlugin):
         sender = message_info.sender
         re_obj = message_parameter.get("re_obj")
         title = re_obj.groups(1)
-        message = MessageSegment.video(self.get_girl_url(title))
+        # message = MessageSegment.video(self.get_girl_url(title))
+        message = MessageSegment.video(f"https://api.caonm.net/api/cdmn/m?lx={title}&key=d73IGg5Nn4hXl0a8CzHeUrGUgV")
         if message_info.get("message_type") == "group":
             await SendGroupMsgRequest(group_id=message_info.get("group_id"), message=message).send_request(
                 CQApiConfig.message.send_group_msg.Api
