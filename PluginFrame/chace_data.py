0518d1337c95516cb308b0a3991143e4a7d6d86f
BXb <1113855149@qq.com>
2023-06-28 18:42:23 +0800

    修改

--- PluginFrame/chace_data.py
+++ PluginFrame/chace_data.py
@@ -3,7 +3,7 @@ import os
 import time
 
 cache_data = {}
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.dirname(os.path.dirname(os.path.abspath(os.path.abspath(os.path.abspath(__file__)))))
 
 
 def init_cache():
