b5e9376c531f2da21a94424d67945b67edf13d90
BXb <1113855149@qq.com>
2023-06-29 14:38:01 +0800

    修改

--- PluginFrame/Plugins/image/image_variation.py
+++ PluginFrame/Plugins/image/image_variation.py
@@ -85,8 +85,7 @@ class BlackSilkPlugin(BaseComponentPlugin):
         event = message_parameter.get("event")
         bot = message_parameter.get("bot")
         re_obj = message_parameter.get("re_obj")
-        title = re_obj.group(0)
-        print(title)
+        title = re_obj.group(1)
         if title == "黑丝":
             url = "http://api.caonm.net/api/bhs/h?key=d73IGg5Nn4hXl0a8CzHeUrGUgV"
         else:
